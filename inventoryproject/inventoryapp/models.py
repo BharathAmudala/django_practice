931ca90628a2f92a767aede057f598ff742fcf1d
BharathAmudala <amudala.bharath98@gmail.com>
2025-07-20 01:03:43 +0530

    Implement initial models, serializers, views, and URLs for inventory management app

--- inventoryproject/inventoryapp/models.py
+++ inventoryproject/inventoryapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'Product'
+
+class Stock(models.Model):
+    name = models.CharField(max_length=250)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'Stock'
+
+class Purchase(models.Model):
+    Product = models.ForeignKey(Product, db_column='Product_id', on_delete = models.CASCADE)
+    quantity_purchased = models.IntegerField()
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date =  models.DateTimeField()
+
+    class Meta:
+        db_table = 'Purchase'
+
+class Sale(models.Model):
+    Product = models.ForeignKey(Product, db_column='Product_id', on_delete = models.CASCADE)
+    quantity_sold = models.IntegerField()
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'Sale'
