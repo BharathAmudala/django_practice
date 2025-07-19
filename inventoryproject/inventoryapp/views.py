e5702b2bedb777bd0a62ec981a8ff2c511af809f
BharathAmudala <amudala.bharath98@gmail.com>
2025-07-20 01:06:23 +0530

    Implement viewsets for Product, Stock, Purchase, and Sale models

--- inventoryproject/inventoryapp/views.py
+++ inventoryproject/inventoryapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+# import requests
+from rest_framework import viewsets
+from .models import Product, Stock, Purchase, Sale
+from .serializers import ProductSerializer, StockSerializer, PurchaseSerializer, SaleSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+class PurchaseViewSet(viewsets.ModelViewSet):
+    queryset = Purchase.objects.all()
+    serializer_class = PurchaseSerializer
+
+
+
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer   
